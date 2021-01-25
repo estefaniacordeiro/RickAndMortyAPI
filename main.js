0f7c47b5bfd0841ef7cd83197a2ebd24991ef14a
Estefanía Cordeiro Brión <estefania.cbrion@gmail.com>
2021-01-25 01:21:38 +0100

    Flow to next done

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ loadHtml();
 
 $('.btn-activeSidebar-icon').on('click', showSidebar);
 
+
+
 function loadHtml() {
     $('body').append(
         `<header class="header">
@@ -39,16 +41,67 @@ function showSidebar() {
 }
 
 function requestAPI() {
-    axios.get('https://rickandmortyapi.com/api/episode').then((data) => {
-        console.log(data);
-        let page1Episodes = data.data.results;
-        console.log(page1Episodes);
+    /* Data of page 1 */
+    axios.get('https://rickandmortyapi.com/api/episode').then((dataPage1) => {
+        console.log(dataPage1);
+        let page1Episodes = dataPage1.data.results;
         page1Episodes.forEach(episode => {
             $('.sectionContent-sidebar-listEpisodes').append(
                 `<li>
                     <button class="btn-episode">Episode ${episode.id}</button>
                 </li>`);
-            console.log(episode);
         });
+        /* Data of page 2 */
+        $('.sectionContent-sidebar-btnPages-next').on('click', () => {
+            if(dataPage1.data.info.prev === null) {
+                $('.sectionContent-sidebar-btnPages-back').show();
+            }
+            /* Return page 1 */
+            $('.sectionContent-sidebar-btnPages-back').on('click',() => {
+                console.log('como recupero la lista de capítulos de la página 1?')
+            })
+            /* Show the new episodes page 2*/
+            let nextUrl = dataPage1.data.info.next;
+            axios.get(nextUrl).then((dataPage2) => {
+                console.log(dataPage2);
+                $('.sectionContent-sidebar-listEpisodes')[0].textContent = '';
+                const page2Episodes = dataPage2.data.results;
+                page2Episodes.forEach(episode => {
+                    $('.sectionContent-sidebar-listEpisodes').append(
+                        `<li>
+                            <button class="btn-episode">Episode ${episode.id}</button>
+                        </li>`);
+                });
+
+                /* Data of page 3 */
+                $('.sectionContent-sidebar-btnPages-next').on('click', () => {
+                    /* Return page 2 */
+                    $('.sectionContent-sidebar-btnPages-back').on('click',() => {
+                        console.log('como recupero la lista de capítulos de la página 2?')
+                    })
+                    /* Show the new episodes page 3*/
+                    let nextUrl = dataPage2.data.info.next;
+                    axios.get(nextUrl).then((dataPage3) => {
+                        console.log(dataPage3);
+                        $('.sectionContent-sidebar-listEpisodes')[0].textContent = '';
+                        if(dataPage3.data.info.next === null) {
+                            $('.sectionContent-sidebar-btnPages-next').hide();
+                        }
+                        const page3Episodes = dataPage3.data.results;
+                        page3Episodes.forEach(episode => {
+                            $('.sectionContent-sidebar-listEpisodes').append(
+                                `<li>
+                                    <button class="btn-episode">Episode ${episode.id}</button>
+                                </li>`);
+                        });
+                    })
+                });
+            });
+        });
+    $('.btn-episode').on('click', showInfoEpisode);
     });
+}
+
+function showInfoEpisode() {
+    $('.sectionContent-main').css('background-image', 'none');
 }
